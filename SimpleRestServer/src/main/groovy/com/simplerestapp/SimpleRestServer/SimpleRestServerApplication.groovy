97ceef06f4a54cd65fb7a1bfbd78dfb7a2e0ab68
Ian Hall <ian.hall.ch@gmail.com>
2020-07-14 11:24:40 -0600

    Initial commit. Created SimpleRestServer spring boot application.

---
+++ SimpleRestServer/src/main/groovy/com/simplerestapp/SimpleRestServer/SimpleRestServerApplication.groovy
@@ -0,0 +1,13 @@
+package com.simplerestapp.SimpleRestServer
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class SimpleRestServerApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(SimpleRestServerApplication, args)
+	}
+
+}
---
+++ SimpleRestServer/src/test/groovy/com/simplerestapp/SimpleRestServer/SimpleRestServerApplicationTests.groovy
@@ -0,0 +1,13 @@
+package com.simplerestapp.SimpleRestServer
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SimpleRestServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
